718a3c36f8defb866a4827a1fd81cc8bf783cf0a
Nathaniel Nielsen <nielsnatd98@outlook.com>
2025-08-27 04:48:15 -0600

    Linked SFML and create CMake File

---
+++ build/CMakeFiles/FoilSimulation.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FoilSimulation.dir/main.cpp.o"
+  "CMakeFiles/FoilSimulation.dir/main.cpp.o.d"
+  "FoilSimulation"
+  "FoilSimulation.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FoilSimulation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
